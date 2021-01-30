a1bf7c06a0fb70b5a256a16dae0ade4461f3eb36
satya9500 <satya.prakash9500@gmail.com>
2021-01-30 13:56:53 +0530

    recordin audio from UI successful

---
+++ public/index.js
@@ -0,0 +1,34 @@
+
+$('#main-div2').fadeOut('fast');
+var fun1 = function () {
+	document.getElementById('label').innerHTML = "";
+	// alert("abcd");
+	$('#btn').prop('disabled', 'true');
+	$('#main-div').fadeOut(700);
+
+	setTimeout(function () {
+		$('#main-div2').fadeIn('slow');
+	}, 800);
+	setTimeout(function () {
+		$('#main-div2').fadeOut(700);
+	}, 5800);
+	setTimeout(function () {
+		$('#main-div3').fadeIn('slow');
+	}, 6550);
+
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			$('#btn').removeAttr('disabled');
+			setTimeout(function () {
+				$('#main-div3').fadeOut(700);
+			}, 0);
+			setTimeout(function () {
+				$('#main-div').fadeIn('slow');
+			}, 800);
+			document.getElementById('label').innerHTML = xhttp.responseText;
+		}
+	};
+	xhttp.open("GET", "http://127.0.0.1:5000/detect", true);
+	xhttp.send();
+}
